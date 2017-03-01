c9f9cb7b63c011c5397585214b5a4e48159cce34
梁世茂 <liangshimao@5i5j.com>
2017-03-01 13:21:36 +0800

    adfs

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="renderer" content="webkit|ie-comp|ie-stand">
+    <meta http-equiv="X-UA-Compatible" content="IE=8">
     <title>优悦佳品后台管理系统</title>
     <script src="/js/jquery-1.9.1.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
